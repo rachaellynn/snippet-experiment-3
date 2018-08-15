aee1aa6a8b40c2fe588648e9368dc83b5bc922af
rachaellynn <rachael@wordaroundtown.com>
2018-08-14 21:43:30 -0400

    added vars

--- public/someapp.js
+++ public/someapp.js
@@ -3,12 +3,12 @@ window.onload = function(){
 //parse the query string
 var queryString = window.location.search;
 queryString = queryString.substring(1);
-params=queryString.split("=");
+var params=queryString.split("=");
 console.log(params[1]);
-name = params[1];
+var name = params[1];
 
 //add some text to the "mytext" div
-mytext = document.getElementById("mytext");
-newtext = document.createTextNode("new text" + " " + name)
+var mytext = document.getElementById("mytext");
+var newtext = document.createTextNode("new text" + " " + name)
 mytext.appendChild(newtext);
 }
