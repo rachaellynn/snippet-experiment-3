1966f10c798ff76a1fb051f09023fb88bb40d2a6
rachaellynn <rachael@wordaroundtown.com>
2018-08-14 21:58:09 -0400

    remove vars

--- public/someapp.js
+++ public/someapp.js
@@ -1,14 +1,14 @@
 
 window.onload = function(){
 //parse the query string
-var queryString = window.location.search;
+queryString = window.location.search;
 queryString = queryString.substring(1);
-var params=queryString.split("=");
+params=queryString.split("=");
 console.log(params[1]);
-var name = params[1];
+name = params[1];
 
 //add some text to the "mytext" div
-var mytext = document.getElementById("mytext");
-var newtext = document.createTextNode("new text" + " " + name)
+mytext = document.getElementById("mytext");
+newtext = document.createTextNode("new text" + " " + name)
 mytext.appendChild(newtext);
 }
